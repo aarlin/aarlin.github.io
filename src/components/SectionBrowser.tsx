9f68cbdb6170aecb103516b1a13469d8fc5a44c9
Aaron Lin <aaronlin256@gmail.com>
2019-10-16 22:36:40 -0400

    Fix tabs using gatsby link, activeClassName

--- src/components/SectionBrowser.tsx
+++ src/components/SectionBrowser.tsx
@@ -1,18 +1,21 @@
 import React from 'react'
 import { Menu, Segment } from 'semantic-ui-react'
 import { NavLink, BrowserRouter } from 'react-router-dom'
+import { Link } from 'gatsby'
 
 const SectionBrowser: React.FC = ({ children }) => {
   return (
-    <BrowserRouter>
-      <Menu attached="top" tabular>
-        <Menu.Item as={NavLink} exact to="/about" name="about" />
-        <Menu.Item as={NavLink} to="/projects" name="projects" />
-        <Menu.Item as={NavLink} to="/blog" name="blog" />
-      </Menu>
+    <>
+      <BrowserRouter>
+        <Menu attached="top" tabular>
+          <Menu.Item as={Link} activeClassName="active" to="/" name="about" />
+          <Menu.Item as={Link} activeClassName="active" to="/projects" name="projects" />
+          <Menu.Item as={Link} activeClassName="active" to="/blog" name="blog" />
+        </Menu>
+      </BrowserRouter>
 
       <Segment attached="bottom">{children}</Segment>
-    </BrowserRouter>
+    </>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react'
 import IndexLayout from '../layouts'
 
-const IndexPage = () => <IndexLayout />
+const IndexPage = () => (
+  <IndexLayout>
+    <div style={{ color: `teal` }}>
+      <h1>About</h1>
+      <p>Such wow. Very React.</p>
+    </div>
+  </IndexLayout>
+)
 
 export default IndexPage
