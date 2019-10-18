07a23acf5a365c90dfac7f5bf02f3c94ef712805
Aaron Lin <aaronlin256@gmail.com>
2019-10-18 18:46:39 -0400

    Fix up BorderedImage file

--- src/components/BorderedImage.tsx
+++ src/components/BorderedImage.tsx
@@ -1,20 +1,10 @@
 import React from 'react'
-import { Image, Card, Icon, List } from 'semantic-ui-react'
-import Img from 'gatsby-image'
+import { Image, Card } from 'semantic-ui-react'
 import { graphql, useStaticQuery } from 'gatsby'
-import SocialMediaButtons from './SocialMediaButtons'
 
 interface BorderedImageProps {
   className?: string
 }
-
-// interface BorderedImageData {
-//   file: {
-//     childImageSharp: {
-//       fixed:
-//     }
-//   }
-// }
 
 const BorderedImage: React.SFC<BorderedImageProps> = ({ className }) => {
   const data = useStaticQuery(graphql`
@@ -39,30 +29,6 @@ const BorderedImage: React.SFC<BorderedImageProps> = ({ className }) => {
           <Card.Meta>
             <span className="job-title">Associate Software Engineer @ Fidelity</span>
           </Card.Meta>
-          <Card.Description>
-            <List>
-              <List.Item>
-                <List.Icon name="users" />
-                <List.Content>Semantic UI</List.Content>
-              </List.Item>
-              <List.Item>
-                <List.Icon name="marker" />
-                <List.Content>New York, NY</List.Content>
-              </List.Item>
-              <List.Item>
-                <List.Icon name="mail" />
-                <List.Content>
-                  <a href="mailto:jack@semantic-ui.com">aaron@semantic-ui.com</a>
-                </List.Content>
-              </List.Item>
-              <List.Item>
-                <List.Icon name="linkify" />
-                <List.Content>
-                  <a href="http://www.semantic-ui.com">semantic-ui.com</a>
-                </List.Content>
-              </List.Item>
-            </List>
-          </Card.Description>
         </Card.Content>
       </Card>
     </>
